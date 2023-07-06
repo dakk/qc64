81dfaf3458baa46ef3801b2382dab0196a0d8a90
Dennis Williamson <6640557+dtwilliamson@users.noreply.github.com>
2023-07-06 12:20:43 -0500

    cleanup and simplification

--- qc.py
+++ qc.py
@@ -5,13 +5,17 @@
 import os
 from math import sqrt
 from random import random
+from itertools import chain
 
 class QC:
+    # naively provide a place to store stuff
+    COUNT = 4
     real = [1, 0, 0, 0]
     imaginary = [0, 0, 0, 0]
     z = [0, 0, 0, 0]
     p = [0, 0, 0, 0]
     a = [0, 0]
+    b = [0, 0]
     sq = 0
 
 qc = QC()
@@ -29,7 +33,8 @@ class Clear:
 
 clear = Clear()
 
-def simulate(gate):
+def simulate_OLD(gate):
+    # a dispatch would be more concise
     match gate:
         case "x0":
             x0()
@@ -52,7 +57,12 @@ def simulate(gate):
         case "sw":
             sw()
 
-# 400
+def simulate(gate):
+    # use a dispatch table to select function to run
+    gates = (x0, x1, y0, y1, z0, z1, h0, h1, cx, sw)
+    funcs = {g.__name__: g for g in gates}
+    funcs[gate]()
+
 def x0():
     qc.a[0] = qc.real[0]; qc.real[0] = qc.real[1]; qc.real[1] = qc.a[0]
     qc.a[0] = qc.imaginary[0]; qc.imaginary[0] = qc.imaginary[1]; qc.imaginary[1] = qc.a[0]
@@ -60,15 +70,11 @@ def x0():
     qc.a[0] = qc.imaginary[2]; qc.imaginary[2] = qc.imaginary[3]; qc.imaginary[3] = qc.a[0]
     return
 
-# 440
 def y0():
-    qc.a[0] = qc.imaginary[0]; qc.imaginary[0] = -qc.real[0]; qc.real[0] = qc.a[0]
-    qc.a[0] = qc.imaginary[1]; qc.imaginary[1] = -qc.real[1]; qc.real[1] = qc.a[0]
-    qc.a[0] = qc.imaginary[2]; qc.imaginary[2] = -qc.real[2]; qc.real[2] = qc.a[0]
-    qc.a[0] = qc.imaginary[3]; qc.imaginary[3] = -qc.real[3]; qc.real[3] = qc.a[0]
+    for n in range(qc.COUNT):
+        qc.a[0] = qc.imaginary[n]; qc.imaginary[n] = -qc.real[n]; qc.real[n] = qc.a[0]
     return
 
-# 420
 def x1():
     qc.a[0] = qc.real[1]; qc.real[1] = qc.real[3]; qc.real[3] = qc.a[0]
     qc.a[0] = qc.imaginary[1]; qc.imaginary[1] = qc.imaginary[3]; qc.imaginary[3] = qc.a[0]
@@ -77,80 +83,63 @@ def x1():
     y0() # "fall through"? https://github.com/dakk/qc64/issues/5
     return
 
-# 460
 def y1():
     qc.a[0] = qc.imaginary[1]; qc.imaginary[1] = -qc.real[1]; qc.real[1] = qc.a[0]
     qc.a[0] = qc.imaginary[3]; qc.imaginary[3] = -qc.real[3]; qc.real[3] = qc.a[0]
     return
 
-# 480
 def z0():
     qc.imaginary[2] = -qc.imaginary[2]; qc.imaginary[3] = -qc.imaginary[3]
     return
 
-# 500
 def z1():
     qc.imaginary[1] = -qc.imaginary[1]; qc.imaginary[3] = -qc.imaginary[3]
     return
 
-# 520
 def h0():
     qc.a[0] = (qc.real[0] + qc.real[1]) / sqrt(2); qc.a[1] = (qc.imaginary[0] + qc.imaginary[1]) / sqrt(2)
-    b0 = (qc.real[0] - qc.real[1]) / sqrt(2); b1 = (qc.imaginary[0] - qc.imaginary[1]) / sqrt(2)
-    qc.real[0] = qc.a[0]; qc.imaginary[0] = qc.a[1]; qc.real[1] = b0; qc.imaginary[1] = b1
+    qc.b[0] = (qc.real[0] - qc.real[1]) / sqrt(2); qc.b[1] = (qc.imaginary[0] - qc.imaginary[1]) / sqrt(2)
+    qc.real[0] = qc.a[0]; qc.imaginary[0] = qc.a[1]; qc.real[1] = qc.b[0]; qc.imaginary[1] = qc.b[1]
     qc.a[0] = (qc.real[2] + qc.real[3]) / sqrt(2); qc.a[1] = (qc.imaginary[2] + qc.imaginary[3]) / sqrt(2)
-    b0 = (qc.real[2] - qc.real[3]) / sqrt(2); b1 = (qc.imaginary[2] - qc.imaginary[3]) / sqrt(2)
-    qc.real[2] = qc.a[0]; qc.imaginary[2] = qc.a[1]; qc.real[3] = b0; qc.imaginary[3] = b1
+    qc.b[0] = (qc.real[2] - qc.real[3]) / sqrt(2); qc.b[1] = (qc.imaginary[2] - qc.imaginary[3]) / sqrt(2)
+    qc.real[2] = qc.a[0]; qc.imaginary[2] = qc.a[1]; qc.real[3] = qc.b[0]; qc.imaginary[3] = qc.b[1]
     return
 
-# 540
 def h1():
     qc.a[0] = (qc.real[0] + qc.real[2]) / sqrt(2); qc.a[1] = (qc.imaginary[0] + qc.imaginary[2]) / sqrt(2)
-    b0 = (qc.real[0] - qc.real[2]) / sqrt(2); b1 = (qc.imaginary[0] - qc.imaginary[2]) / sqrt(2)
-    qc.real[0] = qc.a[0]; qc.imaginary[0] = qc.a[1]; qc.real[2] = b0; qc.imaginary[2] = b1
+    qc.b[0] = (qc.real[0] - qc.real[2]) / sqrt(2); qc.b[1] = (qc.imaginary[0] - qc.imaginary[2]) / sqrt(2)
+    qc.real[0] = qc.a[0]; qc.imaginary[0] = qc.a[1]; qc.real[2] = qc.b[0]; qc.imaginary[2] = qc.b[1]
     qc.a[0] = (qc.real[1] + qc.real[3]) / sqrt(2); qc.a[1] = (qc.imaginary[1] + qc.imaginary[3]) / sqrt(2)
-    b0 = (qc.real[1] - qc.real[3]) / sqrt(2); b1 = (qc.imaginary[1] - qc.imaginary[3]) / sqrt(2)
-    qc.real[1] = qc.a[0]; qc.imaginary[1] = qc.a[1]; qc.real[3] = b0; qc.imaginary[3] = b1
+    qc.b[0] = (qc.real[1] - qc.real[3]) / sqrt(2); qc.b[1] = (qc.imaginary[1] - qc.imaginary[3]) / sqrt(2)
+    qc.real[1] = qc.a[0]; qc.imaginary[1] = qc.a[1]; qc.real[3] = qc.b[0]; qc.imaginary[3] = qc.b[1]
     return
 
-# 560
 def cx():
     qc.a[0] = qc.real[1]; qc.real[1] = qc.real[3]; qc.real[3] = qc.a[0]
     qc.a[0] = qc.imaginary[1]; qc.imaginary[1] = qc.imaginary[3]; qc.imaginary[3] = qc.a[0]
     return 
 
-# 580
 def sw():
     qc.a[0] = qc.real[1]; qc.real[1] = qc.real[2]; qc.real[2] = qc.a[0]
     qc.a[0] = qc.imaginary[1]; qc.imaginary[1] = qc.imaginary[2]; qc.imaginary[2] = qc.a[0]
     return
 
-# 600
 def statevcector_normalization():
     nf = sqrt(1 / qc.sq)
-    qc.real[0] = qc.real[0] * nf
-    qc.imaginary[0] = qc.imaginary[0] * nf
-    qc.real[1] = qc.real[1] * nf
-    qc.imaginary[1] = qc.imaginary[1] * nf
-    qc.real[2] = qc.real[2] * nf
-    qc.imaginary[2] = qc.imaginary[2] * nf
-    qc.real[3] = qc.real[3] * nf
-    qc.imaginary[3] = qc.imaginary[3] * nf
+    for n in range(qc.COUNT):
+        qc.real[n] *= nf
+        qc.imaginary[n] *= nf
     return
 
 def results():
     print("results:")
-    print(f'00: [{qc.z[0]}] {qc.z[0] * "Q"}')
-    print(f'01: [{qc.z[2]}] {qc.z[2] * "Q"}')
-    print(f'10: [{qc.z[1]}] {qc.z[1] * "Q"}')
-    print(f'11: [{qc.z[3]}] {qc.z[3] * "Q"}')
+    b = 0
+    # evens then odds
+    for n in chain(range(0, qc.COUNT, 2), range(1, qc.COUNT, 2)):
+        print(f'{b:02b}: [{qc.z[n]}] {qc.z[n] * "Q"}')
+        b += 1
 
 def main():
-    '''
-    qc.real[0] = 1; qc.imaginary[0] = 0; qc.real[1] = 0; qc.imaginary[1] = 0
-    qc.real[2] = 0; qc.imaginary[2] = 0; qc.real[3] = 0; qc.imaginary[3] = 0
-    qc.a[0] = 0
-    '''
     shots = 28
     clear()
     print("quantum simulator")
@@ -164,22 +153,23 @@ def main():
         print(".", end = '')
     print()
 
-    qc.sq = qc.real[0]*qc.real[0] + qc.imaginary[0]*qc.imaginary[0] + qc.real[1]*qc.real[1] + qc.imaginary[1]*qc.imaginary[1] + qc.real[2]*qc.real[2] + qc.imaginary[2]*qc.imaginary[2] + qc.real[3]*qc.real[3] + qc.imaginary[3]*qc.imaginary[3]
+    qc.sq = sum([x ** 2 for x in qc.real] +
+                [x ** 2 for x in qc.imaginary])
+
     if abs(qc.sq - 1) > 0.00001: statevcector_normalization()
 
     print(f"running {shots} iterations...")
-    # qc.z[0] = 0; qc.z[1] = 0; qc.z[2] = 0; qc.z[3] = 0
-    qc.p[0] = (qc.real[0] * qc.real[0] + qc.imaginary[0] * qc.imaginary[0])
-    qc.p[1] = (qc.real[1] * qc.real[1] + qc.imaginary[1] * qc.imaginary[1]) + qc.p[0]
-    qc.p[2] = (qc.real[2] * qc.real[2] + qc.imaginary[2] * qc.imaginary[2]) + qc.p[1]
-    qc.p[3] = (qc.real[3] * qc.real[3] + qc.imaginary[3] * qc.imaginary[3]) + qc.p[2]
+    prev = 0
+    for n in range(qc.COUNT):
+        qc.p[n] = qc.real[n] ** 2 + qc.imaginary[n] ** 2 + prev
+        prev = qc.p[n]
 
     for i in range(shots):
         r = random()
-        if r < qc.p[0]: qc.z[0] = qc.z[0] + 1
-        if r >= qc.p[0] and r < qc.p[1]: qc.z[1] = qc.z[1] + 1
-        if r >= qc.p[1] and r < qc.p[2]: qc.z[2] = qc.z[2] + 1
-        if r >= qc.p[2] and r < qc.p[3]: qc.z[3] = qc.z[3] + 1
+        if r < qc.p[0]: qc.z[0] += 1
+        elif r >= qc.p[0] and r < qc.p[1]: qc.z[1] += 1
+        elif r >= qc.p[1] and r < qc.p[2]: qc.z[2] += 1
+        elif r >= qc.p[2] and r < qc.p[3]: qc.z[3] += 1
 
 if __name__ == '__main__':
     main()
