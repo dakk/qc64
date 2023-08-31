7af98109b09f331472479ee87ac3b505440bdd9e
Ernst-Georg Schmid <333776+ergo70@users.noreply.github.com>
2023-08-31 15:23:12 +0200

    Update qf.f90
    % Now using intrinsic COMPLEX numbers
    % Output in % instead of absolute numbers
    + Number of shots can be chosen

--- qf.f90
+++ qf.f90
@@ -2,8 +2,9 @@
     implicit none
     private
 
+    real, parameter :: s = sqrt(2.0)
     type, public :: t_qc
-        real, dimension(4) :: i, r = (/ 1.0, 0.0, 0.0, 0.0 /)
+        complex, dimension(4) :: c = (/ (1.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0) /)
         real :: sq
     CONTAINS
     PROCEDURE, PASS :: gate_x0
@@ -24,37 +25,25 @@
     subroutine gate_x0(this)
     implicit none
     CLASS(t_qc), INTENT(inout) :: this
-    real :: tmp
-    tmp = this%r(1)
-    this%r(1) = this%r(2)
-    this%r(2) = tmp
-    tmp = this%i(1)
-    this%i(1) = this%i(2)
-    this%i(2) = tmp
-    tmp = this%r(3)
-    this%r(3) = this%r(4)
-    this%r(4) = tmp
-    tmp = this%i(3)
-    this%i(3) = this%i(4)
-    this%i(4) = tmp
+    complex :: tmp
+    tmp = this%c(1)
+    this%c(1) = this%c(2)
+    this%c(2) = tmp
+    tmp = this%c(3)
+    this%c(3) = this%c(4)
+    this%c(4) = tmp
     end subroutine gate_x0
 
     subroutine gate_x1(this)
     implicit none
     CLASS(t_qc), INTENT(inout) :: this
-    real :: tmp
-    tmp = this%r(2)
-    this%r(2) = this%r(4)
-    this%r(4) = tmp
-    tmp = this%i(2)
-    this%i(2) = this%i(4)
-    this%i(4) = tmp
-    tmp = this%r(1)
-    this%r(1) = this%r(3)
-    this%r(3) = tmp
-    tmp = this%i(1)
-    this%i(1) = this%i(3)
-    this%i(3) = tmp
+    complex :: tmp
+    tmp = this%c(2)
+    this%c(2) = this%c(4)
+    this%c(4) = tmp
+    tmp = this%c(1)
+    this%c(1) = this%c(3)
+    this%c(3) = tmp
 
     call this%gate_y0() ! Fallthrough in original code
     end subroutine gate_x1
@@ -65,9 +54,9 @@
     real :: tmp
     integer :: i
     do i=1, 4
-        tmp = this%i(i)
-        this%i(i) = -this%r(i)
-        this%r(i) = tmp
+        tmp = this%c(i)%im
+        this%c(i)%im = -this%c(i)%im
+        this%c(i)%im = tmp
     end do
     end subroutine gate_y0
 
@@ -75,97 +64,72 @@
     implicit none
     CLASS(t_qc), INTENT(inout) :: this
     real :: tmp
-    tmp = this%i(2)
-    this%i(2) = -this%r(2)
-    this%r(1) = tmp
-    tmp = this%i(4)
-    this%i(4) = -this%r(4)
-    this%r(4) = tmp
+    tmp = this%c(2)%im
+    this%c(2)%im = -this%c(2)%re
+    this%c(1)%re = tmp
+    tmp = this%c(4)%im
+    this%c(4)%im = -this%c(4)%re
+    this%c(4)%re = tmp
     end subroutine gate_y1
 
     subroutine gate_z0(this)
     implicit none
     CLASS(t_qc), INTENT(inout) :: this
-    this%i(3) = -this%i(3)
-    this%i(4) = -this%i(4)
+    this%c(3) = CONJG(this%c(3))
+    this%c(4) = CONJG(this%c(4))
     end subroutine gate_z0
 
     subroutine gate_z1(this)
     implicit none
     CLASS(t_qc), INTENT(inout) :: this
-    this%i(2) = -this%i(2)
-    this%i(4) = -this%i(4)
+    this%c(2) = CONJG(this%c(2))
+    this%c(4) = CONJG(this%c(4))
     end subroutine gate_z1
 
     subroutine gate_h0(this)
     implicit none
     CLASS(t_qc), INTENT(inout) :: this
-    real :: a0, a1, b0, b1
-    integer :: i
-    real, parameter :: s = sqrt(2.0)
-    a0 = (this%r(1) + this%r(2)) / s
-    a1 = (this%i(1) + this%i(2)) / s
-    b0 = (this%r(1) - this%r(2)) / s
-    b1 = (this%i(1) - this%i(2)) / s
-    this%r(1) = a0
-    this%i(1) = a1
-    this%r(2) = b0
-    this%i(2) = b1
-    a0 = (this%r(3) + this%r(4)) / s
-    a1 = (this%i(3) + this%i(4)) / s
-    b0 = (this%r(3) - this%r(4)) / s
-    b1 = (this%i(3) - this%i(4)) / s
-    this%r(3) = a0
-    this%i(3) = a1
-    this%r(4) = b0
-    this%i(4) = b1
+    complex :: tmp_a, tmp_b
+    tmp_a = (this%c(1) + this%c(2)) / s
+    tmp_b = (this%c(1) - this%c(2)) / s
+    this%c(1) = tmp_a
+    this%c(2) = tmp_b
+    tmp_a = (this%c(3) + this%c(4)) / s
+    tmp_b = (this%c(3) - this%c(4)) / s
+    this%c(3) = tmp_a
+    this%c(4) = tmp_b
     end subroutine gate_h0
 
     subroutine gate_h1(this)
     implicit none
     CLASS(t_qc), INTENT(inout) :: this
-    real :: a0, a1, b0, b1
-    real, parameter :: s = sqrt(2.0)
-    a0 = (this%r(1) + this%r(3)) / s
-    a1 = (this%i(1) + this%i(3)) / s
-    b0 = (this%r(1) - this%r(3)) / s
-    b1 = (this%i(1) - this%i(3)) / s
-    this%r(1) = a0
-    this%i(1) = a1
-    this%r(3) = b0
-    this%i(3) = b1
-    a0 = (this%r(2) + this%r(4)) / s
-    a1 = (this%i(2) + this%i(4)) / s
-    b0 = (this%r(2) - this%r(4)) / s
-    b1 = (this%i(2) - this%i(4)) / s
-    this%r(2) = a0
-    this%i(2) = a1
-    this%r(4) = b0
-    this%i(4) = b1
+    complex :: tmp_a, tmp_b
+    tmp_a = (this%c(1) + this%c(3)) / s
+    tmp_b = (this%c(1) - this%c(3)) / s
+    this%c(1) = tmp_a
+    this%c(3) = tmp_b
+    tmp_a = (this%c(2) + this%c(4)) / s
+    tmp_b = (this%c(2) - this%c(4)) / s
+    this%c(2) = tmp_a
+    this%c(4) = tmp_b
     end subroutine gate_h1
 
     subroutine gate_cx(this)
     implicit none
     CLASS(t_qc), INTENT(inout) :: this
-    real :: tmp
-    tmp = this%r(2)
-    this%r(2) = this%r(4)
-    this%r(4) = tmp
-    tmp = this%i(2)
-    this%i(2) = this%i(4)
-    this%i(4) = tmp
+    complex :: tmp
+    tmp = this%c(2)
+    this%c(2) = this%c(4)
+    this%c(4) = tmp
     end subroutine gate_cx
 
     subroutine gate_sw(this)
     implicit none
     CLASS(t_qc), INTENT(inout) :: this
-    real :: tmp
-    tmp = this%r(2)
-    this%r(2) = this%r(3)
-    this%r(3) = tmp
-    tmp = this%i(2)
-    this%i(2) = this%i(3)
-    this%i(3) = tmp
+    complex :: tmp
+    tmp = this%c(2)
+    this%c(2) = this%c(3)
+    this%c(3) = tmp
     end subroutine gate_sw
 
     subroutine normalize(this)
@@ -175,8 +139,7 @@
     integer :: i
     nf = sqrt(1.0/this%sq)
     do i=1, 4
-        this%r(i) = this%r(i) * nf
-        this%i(i) = this%i(i) * nf
+        this%c(i) = this%c(i) * nf
     end do
     end subroutine normalize
 
@@ -193,18 +156,28 @@
     real :: r
     real, dimension(4) :: p
     integer, dimension(4) :: z
-    integer :: i
-    integer, parameter :: shots = 28
+    integer :: shots, i
+    integer, parameter :: default_shots = 100
     character(len = 255) :: gates
     character(len = 2) :: gate
 
     print *, "FORTRAN quantum simulator"
     print *, "Created by ergo70, based on work by davide gessa (dakk)"
-    print *, "Enter gate seq (x0,x1,y0,y1,z0,z1,h0,h1,cx,sw)"
+    write (*, fmt="(A)", advance="no") " Enter gate sequence (x0,x1,y0,y1,z0,z1,h0,h1,cx,sw): "
     read(*, "(A)") gates
-    print *, "Calculating the statevector...";
+    write (*, fmt="(A,I0,A))", advance="no") " Enter desired number of shots, or RETURN for default (", default_shots, "): "
+    read(*, "(I)") shots
+    if (shots <= 0) then
+        shots = default_shots
+    end if
+    print *, "Calculating the statevector..."
 
-    do i = 1, LEN(gates),  2
+    if (0 /= MODULO(LEN_TRIM(gates),  2)) then
+        print *, "Incomplete gate sequence"
+        call EXIT(0)
+    end if
+
+    do i = 1, LEN_TRIM(gates),  2
         gate = gates(i:i+2)
         if (gate == '') then
             exit
@@ -233,22 +206,20 @@
             case default
             print *, gate, "Not implemented"
         end select
-        write(*, fmt="(A)", advance="no") "."
+        !write(*, fmt="(A)", advance="no") "."
     end do
 
-    print *, ''
-
-    qc%sq = qc%r(1)*qc%r(1) + qc%i(1)*qc%i(1) + qc%r(2)*qc%r(2) + qc%i(2)*qc%i(2) + qc%r(3)*qc%r(3) + qc%i(3)*qc%i(3) + qc%r(4)*qc%r(4) + qc%i(4)*qc%i(4)
+    qc%sq = DOT_PRODUCT(qc%c, qc%c)
 
     if (ABS(qc%sq - 1) > 0.00001) then
         call qc%normalize()
     end if
 
     print *, "Running", shots, "iterations..."
-    p(1) = (qc%r(1) * qc%r(1) + qc%i(1) * qc%i(1))
-    p(2) = (qc%r(2) * qc%r(2) + qc%i(2) * qc%i(2)) + p(1)
-    p(3) = (qc%r(3) * qc%r(3) + qc%i(3) * qc%i(3)) + p(2)
-    p(4) = (qc%r(4) * qc%r(4) + qc%i(4) * qc%i(4)) + p(3)
+    p(1) = (qc%c(1)%re ** 2 + qc%c(1)%im ** 2)
+    p(2) = (qc%c(2)%re ** 2 + qc%c(2)%im ** 2) + p(1)
+    p(3) = (qc%c(3)%re ** 2 + qc%c(3)%im ** 2) + p(2)
+    p(4) = (qc%c(4)%re ** 2 + qc%c(4)%im ** 2) + p(3)
 
     !$OMP PARALLEL SHARED(p,z) PRIVATE(r)
     !$OMP DO
@@ -269,27 +240,26 @@
     end do
     !$OMP END PARALLEL
 
-    print *,"Results:"
-    write (*, fmt="(A,I,A)", advance="no"), "00: [", z(1), "]"
-    do i=1, z(1)
+    print *,"Result:"
+    write (*, fmt="(A,F4.2,A)", advance="no"), " 00: [", REAL(z(1))/shots, "]"
+    do i=1, z(1), 10
         write(*, fmt="(A)", advance="no") "#"
     end do
     print *,''
-    write (*, fmt="(A,I,A)", advance="no"), "01: [", z(3), "]"
-    do i=1, z(3)
+    write (*, fmt="(A,F4.2,A)", advance="no"), " 01: [", REAL(z(3))/shots, "]"
+    do i=1, z(3), 10
         write(*, fmt="(A)", advance="no") "#"
     end do
     print *, ''
-    write (*, fmt="(A,I,A)", advance="no"), "10: [", z(2), "]"
-    do i=1, z(2)
+    write (*, fmt="(A,F4.2,A)", advance="no"), " 10: [", REAL(z(2))/shots, "]"
+    do i=1, z(2), 10
         write(*, fmt="(A)", advance="no") "#"
     end do
     print *, ''
-    write (*, fmt="(A,I,A)", advance="no"), "11: [", z(4), "]"
-    do i=1, z(4)
+    write (*, fmt="(A,F4.2,A)", advance="no"), " 11: [", REAL(z(4))/shots, "]"
+    do i=1, z(4), 10
         write(*, fmt="(A)", advance="no") "#"
     end do
     print *, ''
 
     end program QC_F
-
